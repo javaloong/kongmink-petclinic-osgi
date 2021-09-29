bd98bb31f0d15dc408255f5d95cab0abb8f05225
tutunannan <bd66_6@hotmail.com>
2021-09-29 11:54:38 +0800

    feat(security): add external configuration shiro.ini

--- petclinic-rest-auth/src/main/java/org/javaloong/kongmink/petclinic/rest/auth/internal/AuthShiroFilter.java
+++ petclinic-rest-auth/src/main/java/org/javaloong/kongmink/petclinic/rest/auth/internal/AuthShiroFilter.java
@@ -2,16 +2,24 @@ package org.javaloong.kongmink.petclinic.rest.auth.internal;
 
 import org.apache.shiro.config.Ini;
 import org.apache.shiro.web.env.IniWebEnvironment;
-import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.apache.shiro.web.env.WebEnvironment;
+import org.apache.shiro.web.filter.mgt.FilterChainResolver;
 import org.apache.shiro.web.servlet.AbstractShiroFilter;
 import org.javaloong.kongmink.petclinic.rest.RESTConstants;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.http.whiteboard.HttpWhiteboardConstants;
 import org.osgi.service.http.whiteboard.propertytypes.HttpWhiteboardContextSelect;
 import org.osgi.service.http.whiteboard.propertytypes.HttpWhiteboardFilterPattern;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.Filter;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 
 import static org.javaloong.kongmink.petclinic.rest.auth.internal.AuthShiroFilter.CONTEXT_NAME;
 
@@ -20,23 +28,46 @@ import static org.javaloong.kongmink.petclinic.rest.auth.internal.AuthShiroFilte
 @HttpWhiteboardFilterPattern("/*")
 public class AuthShiroFilter extends AbstractShiroFilter {
 
+    private static final Logger log = LoggerFactory.getLogger(AuthShiroFilter.class);
+
     public static final String CONTEXT_NAME  = "context.for" + RESTConstants.JAX_RS_NAME;
 
-    private static final Ini INI_FILE = new Ini();
-
-    static {
-        // Can't use the Ini.fromResourcePath(String) method because it can't find "shiro.ini" on the classpath in an OSGi context
-        INI_FILE.load(AuthShiroFilter.class.getClassLoader().getResourceAsStream("shiro.ini"));
+    @Activate
+    public void activate(BundleContext context) throws Exception {
+        WebEnvironment env = createWebEnvironment(context);
+        this.setSecurityManager(env.getWebSecurityManager());
+        FilterChainResolver resolver = env.getFilterChainResolver();
+        if (resolver != null) {
+            this.setFilterChainResolver(resolver);
+        }
     }
 
-    @Activate
-    public void activate() {
+    private WebEnvironment createWebEnvironment(BundleContext context) throws IOException {
         IniWebEnvironment environment = new IniWebEnvironment();
-        environment.setIni(INI_FILE);
+        Ini ini = createShiroIni(context.getBundle());
+        environment.setIni(ini);
         environment.setServletContext(getServletContext());
         environment.init();
-        DefaultWebSecurityManager securityManager = (DefaultWebSecurityManager) environment.getWebSecurityManager();
-        this.setSecurityManager(securityManager);
-        this.setFilterChainResolver(environment.getFilterChainResolver());
+        return environment;
+    }
+
+    private Ini createShiroIni(Bundle bundle) throws IOException {
+        String shiroIniPath = System.getProperty("karaf.etc") + File.separator + "shiro.ini";
+        File f = new File(shiroIniPath);
+        if(f.exists()) {
+            Ini ini = new Ini();
+            String fileBasedIniPath = f.getAbsolutePath();
+            log.debug("Attempting an ini load from the file: \"{}\"", fileBasedIniPath);
+            ini.loadFromPath(fileBasedIniPath);
+            return ini;
+        }
+        return createShiroIniFromBundle(bundle);
+    }
+
+    private Ini createShiroIniFromBundle(Bundle bundle) throws IOException {
+        Ini ini = new Ini();
+        URL url = bundle.getEntry("shiro.ini");
+        ini.load(url.openStream());
+        return ini;
     }
 }
